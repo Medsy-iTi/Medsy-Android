985d75c789cb35b20cc9abcdbfea865943a75748
Ahmed Mohamed <ahmedmonataf@gmail.com>
2026-07-15 12:52:08 +0300

    feat:design login screen with common component

---
+++ designsystem/src/main/java/com/medsy/designsystem/components/MedsyTextField.kt
@@ -0,0 +1,46 @@
+package com.medsy.designsystem.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.medsy.designsystem.ui.theme.BorderGray
+import com.medsy.designsystem.ui.theme.NeutralWhite
+
+@Composable
+fun MedsyTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    singleLine: Boolean = true
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier.fillMaxWidth(),
+        placeholder = placeholder,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        visualTransformation = visualTransformation,
+        shape = RoundedCornerShape(12.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = BorderGray,
+            focusedContainerColor = NeutralWhite,
+            unfocusedContainerColor = NeutralWhite
+        ),
+        keyboardOptions = keyboardOptions,
+        singleLine = singleLine
+    )
+}
