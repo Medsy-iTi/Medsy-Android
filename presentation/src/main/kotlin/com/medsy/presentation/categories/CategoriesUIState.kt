ffab125100a02560299b9328aab7e8947ff586b5
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2026-07-17 00:16:52 +0300

    feat: Implement Categories Screen MVI and Grid UI

---
+++ presentation/src/main/kotlin/com/medsy/presentation/categories/CategoriesUIState.kt
@@ -0,0 +1,11 @@
+package com.medsy.presentation.categories
+
+import com.medsy.presentation.home.CategoryUi
+
+data class CategoriesUIState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val searchQuery: String = "",
+    val categories: List<CategoryUi> = emptyList(),
+    val filteredCategories: List<CategoryUi> = emptyList()
+)
