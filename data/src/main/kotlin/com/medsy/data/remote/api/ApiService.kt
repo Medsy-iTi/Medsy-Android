7475c18d94b44ed13989be2e2cd74c3a7ce2d14d
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2026-07-17 05:39:38 +0300

    add categories API endpoint

--- data/src/main/kotlin/com/medsy/data/remote/api/ApiService.kt
+++ data/src/main/kotlin/com/medsy/data/remote/api/ApiService.kt
@@ -1,4 +1,16 @@
 package com.medsy.data.remote.api
 
+import com.medsy.data.remote.dtos.categories.CategoriesDataDto
+import com.medsy.data.remote.network.ApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+    @GET("categories")
+    suspend fun getCategories(
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("sort") sort: String
+    ): Response<ApiResponse<CategoriesDataDto>>
 }
