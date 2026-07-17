7517622735268f0a551f8b8e2c4afa60b0218285
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-07-17 22:01:47 +0300

    feat: add prescription domain and data flow

---
+++ data/src/main/kotlin/com/medsy/data/prescription/mock/MockPrescriptionScenario.kt
@@ -0,0 +1,8 @@
+package com.medsy.data.prescription.mock
+
+enum class MockPrescriptionScenario {
+    SUCCESS,
+    UPLOAD_FAILURE,
+    UNREADABLE,
+    NO_MEDICINES,
+}
--- data/src/main/kotlin/com/medsy/data/remote/network/SafeApiCall.kt
+++ data/src/main/kotlin/com/medsy/data/remote/network/SafeApiCall.kt
@@ -2,6 +2,7 @@ package com.medsy.data.remote.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.CancellationException
 import retrofit2.Response
 import java.io.IOException
 
@@ -60,6 +61,9 @@ suspend fun <T> safeApiCall(
                 )
             )
         }
+
+    } catch (exception: CancellationException) {
+        throw exception
 
     } catch (e: IOException) {
 
