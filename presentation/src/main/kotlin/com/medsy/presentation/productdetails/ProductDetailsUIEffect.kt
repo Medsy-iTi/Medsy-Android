a3988150a718695c60f36a09a5de389946366d30
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-07-21 02:58:01 +0300

    feat(product-details): add products to cart

--- presentation/src/main/kotlin/com/medsy/presentation/productdetails/ProductDetailsUIEffect.kt
+++ presentation/src/main/kotlin/com/medsy/presentation/productdetails/ProductDetailsUIEffect.kt
@@ -3,7 +3,6 @@ package com.medsy.presentation.productdetails
 sealed interface ProductDetailsUIEffect {
     data object NavigateBack : ProductDetailsUIEffect
     data object OpenShareSheet : ProductDetailsUIEffect
-    data object NavigateToCart : ProductDetailsUIEffect
     data object NavigateToPharmacistChat : ProductDetailsUIEffect
     data class ShowMessage(val messageRes: Int) : ProductDetailsUIEffect
 }
